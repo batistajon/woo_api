b8cf679469b06d55a20c08a7388badfd4fc4a4d9
Jon Batista <batista.jonathas@gmail.com>
2021-05-16 10:07:17 -0300

    buscando orders via get 4

--- app/Http/Controllers/WooCommerceController.php
+++ app/Http/Controllers/WooCommerceController.php
@@ -87,13 +87,41 @@ class WooCommerceController extends Controller
 
         try {
 
-            $woocommerceModel = new WooCommerce();
-
             $results = $this->woocommerce->get('orders/36158');
 
-            //return response()->json($results);
-
-            $woocommerceModel->slackWebhook($data, $results, $icon_url); //TODO fazer chamada ao woocommerce e inserir dados para esse metodo
+            $dataWebhook = [
+                "username" => $data['user_name'],
+                "channel" => $data['channel_id'],
+                "text" => "Numero do novo pedido: " . $results['id'],
+                "mrkdwn" => true,
+                "icon_url" => $icon_url,
+                "attachments" => [
+                    [
+                        "color" => "#b0c4de",
+                        "title" => "Venda cadastrada por: " . $data['user_name'],
+                        "fallback" => 'fallback teste',
+                        "text" => 'Total da compra: '.$results['total'],
+                        "mrkdwn_in" => [
+                            "fallback",
+                            "text"
+                        ]
+                    ]
+                ]
+            ];
+            $json_string = json_encode($dataWebhook);
+            
+            $slack_call = curl_init(env('SLACK_WEBHOOK_URL'));
+            curl_setopt($slack_call, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($slack_call, CURLOPT_POSTFIELDS, $json_string);
+            curl_setopt($slack_call, CURLOPT_CRLF, true);
+            curl_setopt($slack_call, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($slack_call, CURLOPT_HTTPHEADER, array(
+                "Content-Type: application/json",
+                "Content-Length: " . strlen($json_string))
+            );
+            
+            $result = curl_exec($slack_call);
+            curl_close($slack_call);
 
         } catch (\Exception $e) {
 
